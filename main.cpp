93cf22b9b973d236d5e1066e211dd3f37dd07338
warunyucha <warunyu.cha@student.mahidol.edu>
2025-11-18 04:01:24 +0000

    Updated

--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
 #include<iostream>
 using namespace std;
-
+#include "cstring"
 #include "stack.h"
 
 int main(int argc, char **argv){
     Stack s;
-     s.push(5);
-      s.push(1);
-      s.push(7);
-      s.pop();
-      s.push(6);
-      s.pop();
+     s.push(5); //5
+      s.push(1); // 1 5
+      s.push(7); // 7 1 5
+      s.pop(); // 1 5
+      s.push(6); // 6 1 5
+      s.pop(); // 1 5
 
  /*
   Exercise 1*/
-    /*
+    
     int i;
     for(i=1;i<argc;i++){
-      //  if(  x)  s.pop();
+        if(strcmp(argv[i], "x")==0)  s.pop();
         else s.push(atoi(argv[i]));
     }
-     */
+     
 
 
 
